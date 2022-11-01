25ce39909e7d5e7189a8f20c3e7beb6cd3cbeb20
Drew Layton <dlayton66@gmail.com>
2022-11-01 09:43:20 -0600

    test: Create #register_candidate! test

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe do
   it 'has an array of candidates' do
     expect(race.candidates).to eq []
   end
+
+  describe '#regiser_candidate!' do
+    it 'adds a candidate to candidates array, then returns that candidate' do
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+
+      expect(candidate1.class).to eq Candidate
+      expect(candidate1.name).to eq "Diana D"
+      expect(candidate1.party).to eq :democrat
+
+      expect(race.candidates).to eq [candidate1,candidate2]
+    end
+
+  end
 end
