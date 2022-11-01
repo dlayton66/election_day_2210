8c5302463e1f74f10bfeca53aa3224866f008312
Drew Layton <dlayton66@gmail.com>
2022-11-01 09:52:48 -0600

    test: Create Race #add_race test

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -4,6 +4,8 @@ require './lib/election'
 
 RSpec.describe Election do
   let(:election) {Election.new('2022')}
+  let(:race1) {Race.new("Virginia District 4 Representative")}
+  let(:race2) {Race.new("Texas Governor")}
 
   it 'has a year' do
     expect(election.year).to eq '2022'
@@ -11,5 +13,14 @@ RSpec.describe Election do
 
   it 'stores election races' do
     expect(election.races).to eq []
+  end
+
+  describe '#add_race' do
+    it 'adds race to races array' do
+      election.add_race(race1)
+      election.add_race(race2)
+
+      expect(election.races).to eq [race1,race2]
+    end
   end
 end
