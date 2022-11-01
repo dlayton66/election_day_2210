f4daedf5d9c54cec3c2090c3ec1cd26a5e0f4215
Drew Layton <dlayton66@gmail.com>
2022-11-01 09:30:17 -0600

    test: Create Candidate #vote_for! test

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,7 +1,8 @@
 class Candidate
-  attr_reader :name, :party
+  attr_reader :name, :party, :votes
   def initialize(info)
     @name = info[:name]
     @party = info[:party]
+    @votes = 0
   end
 end
--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Candidate do
   it 'has a party' do
     expect(diana.party).to eq :democrat
   end
+
+  it 'keeps a tally of votes' do
+    expect(diana.votes).to eq 0
+  end
+
+  describe '#vote_for!' do
+    it 'increases votes by 1' do
+      diana.vote_for!
+      diana.vote_for!
+      diana.vote_for!
+      
+      expect(diana.votes).to eq 3
+
+      diana.vote_for!
+      expect(diana.votes).to eq 4
+    end
+  end
 end
